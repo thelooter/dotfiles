c55109fe64c81ccd13e1b00654dbc29d937e8132
thelooter <evekolb2204@gmail.com>
2024-06-05 10:34:08 +0200

    Add tiny-devicons-auto-colors.nvim plugin
    Signed-off-by: thelooter <evekolb2204@gmail.com>

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -630,6 +630,19 @@ function M.setup()
       },
     },
     { "Bilal2453/luvit-meta", lazy = true },
+    {
+      "rachartier/tiny-devicons-auto-colors.nvim",
+      dependencies = {
+        "nvim-tree/nvim-web-devicons",
+      },
+      event = "VeryLazy",
+      config = function()
+        local theme_colors = require("catppuccin.palettes").get_palette("frappe")
+        require("tiny-devicons-auto-colors").setup({
+          colors = theme_colors,
+        })
+      end,
+    },
   })
 end
 
